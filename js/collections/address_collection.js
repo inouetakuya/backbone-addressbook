33efe111298b9be3f6076d024ec2bfeb2ac3a6fd
INOUE Takuya <inouetakuya5@gmail.com>
2014-10-15 17:28:11 +0900

    AddressCollection を実装した

--- js/collections/address_collection.js
+++ js/collections/address_collection.js
@@ -1,5 +1,6 @@
-$(function() {
-  var AddressCollection = Backbone.Collection.extend({
-    // あとで実装する
-  });
-})
+var AddressCollection = Backbone.Collection.extend({
+  model: Address,
+  localStorage: new Store('addressbook-sample')
+});
+
+var Addresses = new AddressCollection;
