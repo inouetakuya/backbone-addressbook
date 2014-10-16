7b86c027f540cbe7203aa0c1f124e9c43b2f0b8a
Takatoshi Ono <takatoshi.ono@gmail.com>
2014-10-16 14:37:06 +0900

    Implement AppView

--- js/views/app_view.js
+++ js/views/app_view.js
@@ -1,5 +1,42 @@
-$(function() {
-  var AppView = Backbone.View.extend({
-    // あとで実装する
-  });
-})
+var AppView = Backbone.View.extend({
+  el: $('#app'),
+
+  events: {
+    'keypress #new-address': 'keyPress',
+    'click #delete-all': 'deleteAll'
+  },
+
+  initialize: function() {
+    this.input = this.$('#new-address');
+
+    Addresses.bind('add', this.add, this);
+    Addresses.bind('reset', this.addAll, this);
+
+    Addresses.fetch();
+  },
+
+  add: function(address) {
+    var view = new AddressView({ model: address });
+    this.$('#list').append(view.render().el);
+  },
+
+  addAll: function() {
+    Addresses.each(this.add);
+  },
+
+  keyPress: function(e) {
+    if (e.keyCode === 13) {
+      Addresses.create({ name: this.input.val() });
+      this.input.val('');
+    }
+  },
+
+  deleteAll: function(e) {
+    var address;
+    while (address = Addresses.first()) {
+      address.destroy();
+    }
+  }
+});
+
+var App = new AppView;
